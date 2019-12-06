7c97e92f27f96ee3448863a7d750b71f979297b7
Zhenia Borodaikevych <borodaykevich@gmail.com>
2019-12-06 06:38:32 +0200

    .

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,10 @@ import { Module } from '@nestjs/common';
 import { CategoriesModule } from './categories/categories.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeOrmConfig } from './config/typeorm.config';
-import { ProductModule } from './product/product.module';
 @Module({
   imports: [
     CategoriesModule,
     TypeOrmModule.forRoot(typeOrmConfig),
-    ProductModule
   ],
   controllers: [],
   providers: []
