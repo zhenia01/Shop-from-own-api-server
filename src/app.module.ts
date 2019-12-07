6b5e728adfee049cebb027b5d7f0107acad9621c
Zhenia Borodaikevych <borodaykevich@gmail.com>
2019-12-08 00:26:01 +0200

    added orders module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,12 +4,14 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeOrmConfig } from './config/typeorm.config';
 import { ProductsModule } from './products/products.module';
 import { AuthModule } from './auth/auth.module';
+import { OrdersModule } from './orders/orders.module';
 @Module({
   imports: [
     CategoriesModule,
     TypeOrmModule.forRoot(typeOrmConfig),
     ProductsModule,
-    AuthModule
+    AuthModule,
+    OrdersModule
   ],
   controllers: [],
   providers: []
